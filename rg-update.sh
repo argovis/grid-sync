5c385c5caf33d9733b673240e1a176b42a5d3eed
billmills <you@example.com>
2024-02-16 15:31:19 -0500

    typo

--- rg-update.sh
+++ rg-update.sh
@@ -1,60 +1,60 @@
-# python translate-rg-grid.py temp total 202206 /tmp/rg/RG_ArgoClim_202206_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202206_2019.nc.gz
-# python translate-rg-grid.py psal total 202206 /tmp/rg/RG_ArgoClim_202206_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202206_2019.nc.gz
-# echo 202206 complete
+python translate-rg-grid.py temp total 202206 /tmp/rg/RG_ArgoClim_202206_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202206_2019.nc.gz
+python translate-rg-grid.py psal total 202206 /tmp/rg/RG_ArgoClim_202206_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202206_2019.nc.gz
+echo 202206 complete
 python translate-rg-grid.py temp total 202207 /tmp/rg/RG_ArgoClim_202207_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202207_2019.nc.gz
-# python translate-rg-grid.py psal total 202207 /tmp/rg/RG_ArgoClim_202207_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202207_2019.nc.gz
-# echo 202207 complete
-# python translate-rg-grid.py temp total 202208 /tmp/rg/RG_ArgoClim_202208_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202208_2019.nc.gz
-# python translate-rg-grid.py psal total 202208 /tmp/rg/RG_ArgoClim_202208_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202208_2019.nc.gz
-# echo 202208 complete
-# python translate-rg-grid.py temp total 202209 /tmp/rg/RG_ArgoClim_202209_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202209_2019.nc.gz
-# python translate-rg-grid.py psal total 202209 /tmp/rg/RG_ArgoClim_202209_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202209_2019.nc.gz
-# echo 202209 complete
-# python translate-rg-grid.py temp total 202210 /tmp/rg/RG_ArgoClim_202210_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202210_2019.nc.gz
-# python translate-rg-grid.py psal total 202210 /tmp/rg/RG_ArgoClim_202210_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202210_2019.nc.gz
-# echo 202210 complete
-# python translate-rg-grid.py temp total 202211 /tmp/rg/RG_ArgoClim_202211_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202211_2019.nc.gz
-# python translate-rg-grid.py psal total 202211 /tmp/rg/RG_ArgoClim_202211_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202211_2019.nc.gz
-# echo 202211 complete
-# python translate-rg-grid.py temp total 202212 /tmp/rg/RG_ArgoClim_202212_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202212_2019.nc.gz
-# python translate-rg-grid.py psal total 202212 /tmp/rg/RG_ArgoClim_202212_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202212_2019.nc.gz
-# echo 202212 complete
-# python translate-rg-grid.py temp total 202301 /tmp/rg/RG_ArgoClim_202301_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202301_2019.nc.gz
-# python translate-rg-grid.py psal total 202301 /tmp/rg/RG_ArgoClim_202301_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202301_2019.nc.gz
-# echo 202301 complete
-# python translate-rg-grid.py temp total 202302 /tmp/rg/RG_ArgoClim_202302_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202302_2019.nc.gz
-# python translate-rg-grid.py psal total 202302 /tmp/rg/RG_ArgoClim_202302_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202302_2019.nc.gz
-# echo 202302 complete
-# python translate-rg-grid.py temp total 202303 /tmp/rg/RG_ArgoClim_202303_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202303_2019.nc.gz
-# python translate-rg-grid.py psal total 202303 /tmp/rg/RG_ArgoClim_202303_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202303_2019.nc.gz
-# echo 202303 complete
-# python translate-rg-grid.py temp total 202304 /tmp/rg/RG_ArgoClim_202304_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202304_2019.nc.gz
-# python translate-rg-grid.py psal total 202304 /tmp/rg/RG_ArgoClim_202304_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202304_2019.nc.gz
-# echo 202304 complete
-# python translate-rg-grid.py temp total 202305 /tmp/rg/RG_ArgoClim_202305_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202305_2019.nc.gz
-# python translate-rg-grid.py psal total 202305 /tmp/rg/RG_ArgoClim_202305_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202305_2019.nc.gz
-# echo 202305 complete
-# python translate-rg-grid.py temp total 202306 /tmp/rg/RG_ArgoClim_202306_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202306_2019.nc.gz
-# python translate-rg-grid.py psal total 202306 /tmp/rg/RG_ArgoClim_202306_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202306_2019.nc.gz
-# echo 202306 complete
-# python translate-rg-grid.py temp total 202307 /tmp/rg/RG_ArgoClim_202307_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202307_2019.nc.gz
-# python translate-rg-grid.py psal total 202307 /tmp/rg/RG_ArgoClim_202307_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202307_2019.nc.gz
-# echo 202307 complete
-# python translate-rg-grid.py temp total 202308 /tmp/rg/RG_ArgoClim_202308_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202308_2019.nc.gz
-# python translate-rg-grid.py psal total 202308 /tmp/rg/RG_ArgoClim_202308_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202308_2019.nc.gz
-# echo 202308 complete
-# python translate-rg-grid.py temp total 202309 /tmp/rg/RG_ArgoClim_202309_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202309_2019.nc.gz
-# python translate-rg-grid.py psal total 202309 /tmp/rg/RG_ArgoClim_202309_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202309_2019.nc.gz
-# echo 202309 complete
-# python translate-rg-grid.py temp total 202310 /tmp/rg/RG_ArgoClim_202310_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202310_2019.nc.gz
-# python translate-rg-grid.py psal total 202310 /tmp/rg/RG_ArgoClim_202310_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202310_2019.nc.gz
-# echo 202310 complete
-# python translate-rg-grid.py temp total 202311 /tmp/rg/RG_ArgoClim_202311_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202311_2019.nc.gz
-# python translate-rg-grid.py psal total 202311 /tmp/rg/RG_ArgoClim_202311_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202311_2019.nc.gz
-# echo 202311 complete
-# python translate-rg-grid.py temp total 202312 /tmp/rg/RG_ArgoClim_202312_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202312_2019.nc.gz
-# python translate-rg-grid.py psal total 202312 /tmp/rg/RG_ArgoClim_202312_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202312_2019.nc.gz
-# echo 202312 complete
-# python translate-rg-grid.py temp total 202401 /tmp/rg/RG_ArgoClim_202401_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202401_2019.nc.gz
-# python translate-rg-grid.py psal total 202401 /tmp/rg/RG_ArgoClim_202401_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202401_2019.nc.gz
-# echo 202401 complete
+python translate-rg-grid.py psal total 202207 /tmp/rg/RG_ArgoClim_202207_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202207_2019.nc.gz
+echo 202207 complete
+python translate-rg-grid.py temp total 202208 /tmp/rg/RG_ArgoClim_202208_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202208_2019.nc.gz
+python translate-rg-grid.py psal total 202208 /tmp/rg/RG_ArgoClim_202208_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202208_2019.nc.gz
+echo 202208 complete
+python translate-rg-grid.py temp total 202209 /tmp/rg/RG_ArgoClim_202209_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202209_2019.nc.gz
+python translate-rg-grid.py psal total 202209 /tmp/rg/RG_ArgoClim_202209_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202209_2019.nc.gz
+echo 202209 complete
+python translate-rg-grid.py temp total 202210 /tmp/rg/RG_ArgoClim_202210_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202210_2019.nc.gz
+python translate-rg-grid.py psal total 202210 /tmp/rg/RG_ArgoClim_202210_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202210_2019.nc.gz
+echo 202210 complete
+python translate-rg-grid.py temp total 202211 /tmp/rg/RG_ArgoClim_202211_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202211_2019.nc.gz
+python translate-rg-grid.py psal total 202211 /tmp/rg/RG_ArgoClim_202211_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202211_2019.nc.gz
+echo 202211 complete
+python translate-rg-grid.py temp total 202212 /tmp/rg/RG_ArgoClim_202212_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202212_2019.nc.gz
+python translate-rg-grid.py psal total 202212 /tmp/rg/RG_ArgoClim_202212_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202212_2019.nc.gz
+echo 202212 complete
+python translate-rg-grid.py temp total 202301 /tmp/rg/RG_ArgoClim_202301_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202301_2019.nc.gz
+python translate-rg-grid.py psal total 202301 /tmp/rg/RG_ArgoClim_202301_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202301_2019.nc.gz
+echo 202301 complete
+python translate-rg-grid.py temp total 202302 /tmp/rg/RG_ArgoClim_202302_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202302_2019.nc.gz
+python translate-rg-grid.py psal total 202302 /tmp/rg/RG_ArgoClim_202302_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202302_2019.nc.gz
+echo 202302 complete
+python translate-rg-grid.py temp total 202303 /tmp/rg/RG_ArgoClim_202303_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202303_2019.nc.gz
+python translate-rg-grid.py psal total 202303 /tmp/rg/RG_ArgoClim_202303_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202303_2019.nc.gz
+echo 202303 complete
+python translate-rg-grid.py temp total 202304 /tmp/rg/RG_ArgoClim_202304_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202304_2019.nc.gz
+python translate-rg-grid.py psal total 202304 /tmp/rg/RG_ArgoClim_202304_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202304_2019.nc.gz
+echo 202304 complete
+python translate-rg-grid.py temp total 202305 /tmp/rg/RG_ArgoClim_202305_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202305_2019.nc.gz
+python translate-rg-grid.py psal total 202305 /tmp/rg/RG_ArgoClim_202305_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202305_2019.nc.gz
+echo 202305 complete
+python translate-rg-grid.py temp total 202306 /tmp/rg/RG_ArgoClim_202306_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202306_2019.nc.gz
+python translate-rg-grid.py psal total 202306 /tmp/rg/RG_ArgoClim_202306_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202306_2019.nc.gz
+echo 202306 complete
+python translate-rg-grid.py temp total 202307 /tmp/rg/RG_ArgoClim_202307_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202307_2019.nc.gz
+python translate-rg-grid.py psal total 202307 /tmp/rg/RG_ArgoClim_202307_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202307_2019.nc.gz
+echo 202307 complete
+python translate-rg-grid.py temp total 202308 /tmp/rg/RG_ArgoClim_202308_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202308_2019.nc.gz
+python translate-rg-grid.py psal total 202308 /tmp/rg/RG_ArgoClim_202308_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202308_2019.nc.gz
+echo 202308 complete
+python translate-rg-grid.py temp total 202309 /tmp/rg/RG_ArgoClim_202309_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202309_2019.nc.gz
+python translate-rg-grid.py psal total 202309 /tmp/rg/RG_ArgoClim_202309_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202309_2019.nc.gz
+echo 202309 complete
+python translate-rg-grid.py temp total 202310 /tmp/rg/RG_ArgoClim_202310_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202310_2019.nc.gz
+python translate-rg-grid.py psal total 202310 /tmp/rg/RG_ArgoClim_202310_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202310_2019.nc.gz
+echo 202310 complete
+python translate-rg-grid.py temp total 202311 /tmp/rg/RG_ArgoClim_202311_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202311_2019.nc.gz
+python translate-rg-grid.py psal total 202311 /tmp/rg/RG_ArgoClim_202311_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202311_2019.nc.gz
+echo 202311 complete
+python translate-rg-grid.py temp total 202312 /tmp/rg/RG_ArgoClim_202312_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202312_2019.nc.gz
+python translate-rg-grid.py psal total 202312 /tmp/rg/RG_ArgoClim_202312_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202312_2019.nc.gz
+echo 202312 complete
+python translate-rg-grid.py temp total 202401 /tmp/rg/RG_ArgoClim_202401_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202401_2019.nc.gz
+python translate-rg-grid.py psal total 202401 /tmp/rg/RG_ArgoClim_202401_2019.nc https://sio-argo.ucsd.edu/pub/www-argo/RG/RG_ArgoClim_202401_2019.nc.gz
+echo 202401 complete
